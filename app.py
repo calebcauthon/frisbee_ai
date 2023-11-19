9d17656a0a2590723a84d48ed3bccc0599685e11
caleb cauthon <caleb.cauthon@pricefx.com>
2023-11-19 13:33:59 -0600

    Add frame limit to UI

--- app.py
+++ app.py
@@ -186,17 +186,23 @@ def get_videos():
 @app.route('/api/annotate', methods=['POST'])
 def annotate_video():
     data = request.get_json()
-    full_path = data.get('path')
-    source_video_path = full_path.split('static', 1)[-1]
+    source_video_path = data.get('source_video_path')
+    source_video_path = source_video_path.split('static', 1)[-1]
     if source_video_path.startswith('//'):
         source_video_path = source_video_path[1:]
+
+    frame_limit = data.get('frame_limit')
+    if frame_limit is not None:
+        frame_limit_flag = f"--frame_limit {frame_limit}"
+    else:
+        frame_limit_flag = ""
 
     source_filename = os.path.basename(source_video_path)
     source_filename = os.path.splitext(source_filename)[0]
     print(f"source_filename: {source_filename} from source_video_path: {source_video_path}")
 
     target_video_path = f'web_ui/static/video_targets/{source_filename}_annotated.mp4'
-    command = f"poetry shell && python process_video.py --source_video_path web_ui/static/{source_video_path} --target_video_path {target_video_path}"
+    command = f"poetry shell && python process_video.py --source_video_path web_ui/static/{source_video_path} --target_video_path {target_video_path} {frame_limit_flag}"
     print(f"Running command: {command}")
     import subprocess
     subprocess.Popen(command, shell=True)
