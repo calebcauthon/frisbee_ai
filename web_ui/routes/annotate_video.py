8762f26dbb5816b642125320ae07035e3b6b8be7
caleb cauthon <caleb.cauthon@pricefx.com>
2023-11-19 13:39:01 -0600

    Refactor

--- web_ui/routes/annotate_video.py
+++ web_ui/routes/annotate_video.py
@@ -6,11 +6,7 @@ def annotate_video_route(dependencies):
     if source_video_path.startswith('//'):
         source_video_path = source_video_path[1:]
 
-    frame_limit = data.get('frame_limit')
-    if frame_limit is not None:
-        frame_limit_flag = f"--frame_limit {frame_limit}"
-    else:
-        frame_limit_flag = ""
+    frame_limit_flag = get_frame_limit(dependencies)
 
     source_filename = dependencies.os.path.basename(source_video_path)
     source_filename = dependencies.os.path.splitext(source_filename)[0]
@@ -20,3 +16,13 @@ def annotate_video_route(dependencies):
     command = f"poetry shell && python process_video.py --source_video_path web_ui/static/{source_video_path} --target_video_path {target_video_path} {frame_limit_flag}"
     dependencies.subprocess.Popen(command, shell=True)
     return {"message": "Video processing started"}, 200
+
+def get_frame_limit(dependencies):
+    data = dependencies.flask.request.get_json()
+    frame_limit = data.get('frame_limit')
+    if frame_limit is not None:
+        frame_limit_flag = f"--frame_limit {frame_limit}"
+    else:
+        frame_limit_flag = ""
+
+    return frame_limit_flag
