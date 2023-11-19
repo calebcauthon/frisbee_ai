05ec1e67805157c60c1a411addd477fa9dd4f273
caleb cauthon <caleb.cauthon@pricefx.com>
2023-11-19 13:52:58 -0600

    Add an annotation file flag

--- web_ui/routes/annotate_video.py
+++ web_ui/routes/annotate_video.py
@@ -1,15 +1,25 @@
 def annotate_video_route(dependencies):
     source_video_path = get_source_video_path(dependencies)
     frame_limit_flag = get_frame_limit(dependencies)
+    annotation_filename_flag = get_annotation_filename(dependencies)
 
     source_filename = dependencies.os.path.basename(source_video_path)
     source_filename = dependencies.os.path.splitext(source_filename)[0]
-    print(f"source_filename: {source_filename} from source_video_path: {source_video_path}")
 
     target_video_path = f'web_ui/static/video_targets/{source_filename}_annotated.mp4'
-    command = f"poetry shell && python process_video.py --source_video_path web_ui/static/{source_video_path} --target_video_path {target_video_path} {frame_limit_flag}"
+    command = f"poetry shell && python process_video.py --source_video_path web_ui/static/{source_video_path} --target_video_path {target_video_path} {frame_limit_flag} {annotation_filename_flag}"
+    print(f"\033[33mRunning {command}\033[0m")
     dependencies.subprocess.Popen(command, shell=True)
     return {"message": "Video processing started"}, 200
+
+def get_annotation_filename(dependencies):
+    flask = dependencies.flask
+    data = flask.request.get_json()
+    annotation_filename = data.get('annotation_file')
+    if annotation_filename is None:
+        return ""
+    else:
+      return f"--annotation_file web_ui/static/annotation_data/{annotation_filename}"
 
 def get_source_video_path(dependencies):
     flask = dependencies.flask
